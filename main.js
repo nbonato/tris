1c24773be44cb903dbe5340af5bca61ef74ff109
nbonato <akatux@gmail.com>
2022-11-09 15:22:08 +0100

    Add logic to check for three in a row

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let container = document.getElementById("container");
-
+let turn = 0;
 const gameBoard = (() => {
     // create a board which is an array of 9 undefined
     const board = Array(9);
@@ -12,10 +12,54 @@ const gameBoard = (() => {
         };
         return true;
     };
+    const checkWin = (index, player) => {
+        let win = (Array(3).fill(player.symbol)).toString();
+        
+        let row = [];
+        if (index < 3) {
+            row = [0,1,2];
+        } else if (index < 6) {
+            row = [3,4,5];
+        } else {
+            row = [6,7,8];
+        };
 
+        if (win === (row.map(i=>board[i])).toString()) {
+            return true;
+        };
 
+        let col = [];
+        if (index % 3 == 0) {
+            col = [0,3,6];
+        } else if (index % 3 == 1) {
+            col = [1,4,7];
+        } else {
+            col = [2,5,8];
+        };
+        if (win === (col.map(i=>board[i])).toString()) {
+            return true;
+        };
+
+        let dia = [];
+        let topLeft = [0,4,8].map(i=>board[i]).toString()
+        let topRight = [2,4,6].map(i=>board[i]).toString()
+        if (index == 4) {
+            if (win == topLeft || win == topRight) {
+                return true;
+            };           
+        } else if (index == 0 || index == 8) {
+            dia = topLeft;
+        } else {
+            dia = topRight;
+        };
+
+        if (win === dia) {
+            return true;
+        };
+    };
+    
     // returning everything into an object
-    return {board, empty};
+    return {board, empty, checkWin};
 })();
  
 const player = (name, symbol) => {
@@ -24,10 +68,17 @@ const player = (name, symbol) => {
         if (gameBoard.empty(index)) {
             gameBoard.board[index] = symbol;
             game.render();
+            turn ++;
+            if (turn > 4) {
+                if(gameBoard.checkWin(index, currentPlayer)) {
+                    console.log("Victory");
+                };
+            };
+            game.switchPlayers();
         };     
     };
     const active = 1;
-    return {playerName, mark, active};
+    return {playerName, mark, active, symbol};
 };
 
 const player1 = player("p1", "x");
@@ -61,7 +112,6 @@ const gameBoardCell = (content, position) => {
     cell.dataset.position = position;
     cell.addEventListener("click", () => {
         currentPlayer.mark(position);
-        game.switchPlayers();
     });
     return{cell};
 };
